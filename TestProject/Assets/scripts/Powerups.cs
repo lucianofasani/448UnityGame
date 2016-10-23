ffdafe37bffb4db474f5e4edd2396d747dcf7ae4
lucianofasani <lucianofasani@gmail.com>
2016-10-23 18:40:54 -0500

    Initial Push

---
+++ TestProject/Assets/scripts/Powerups.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerups : MonoBehaviour {
+
+    private CharacterControllerScript theController;
+
+	// Use this for initialization
+	void Start () {
+        theController = FindObjectOfType<CharacterControllerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "character")
+        {
+            theController.jump = true;
+            Destroy(gameObject);
+        }
+
+    }
+
+}
